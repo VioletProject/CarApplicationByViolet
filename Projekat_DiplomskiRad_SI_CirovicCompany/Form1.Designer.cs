50fc1328a9659d25c0b88ff98d3cc179457aa219
VioletProject <98975291+VioletProject@users.noreply.github.com>
2022-02-07 20:48:34 +0100

    Add files via upload

---
+++ Projekat_DiplomskiRad_SI_CirovicCompany/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Projekat_DiplomskiRad_SI_CirovicCompany
+{
+    partial class Forma1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(400, 106);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CIROVIC COMPANY\r\nprodaja polovnih automobila\r\nTrg narodnog heroja 57, Kragujevac\r" +
+    "\n\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Naziv automobila";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tip - marka automobila";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cena automobila u evrima";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 283);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 19);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "DODAJ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 393);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(502, 212);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "IZMENI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(401, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "IZBRISI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(354, 611);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 31);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "PRIKAZI";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(59, 620);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(269, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "PRIKAZATI TRENUTNU LISTU AUTOMOBILA";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(228, 152);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 19);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(377, 152);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(177, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "CEKIRATI ZA OPCIJU IZMENE";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "ID automobila";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(377, 283);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(188, 17);
+            this.checkBox2.TabIndex = 21;
+            this.checkBox2.Text = "CEKIRATI ZA OPCIJU BRISANJA";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Forma1
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 675);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Forma1";
+            this.Text = "CirovicCompany";
+            this.Load += new System.EventHandler(this.Forma1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Projekat_DiplomskiRad_SI_CirovicCompany/Form1.cs
@@ -0,0 +1,229 @@
+﻿using BusinessLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Projekat_DiplomskiRad_SI_CirovicCompany
+{
+    public partial class Forma1 : Form
+    {
+        private CompanyBusiness companyBusiness;
+
+
+        public Forma1()
+        {
+            InitializeComponent();
+            this.companyBusiness = new CompanyBusiness();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Molim vas da popunite sva polja.");
+                textBox1.Focus();
+                return;
+            }
+
+            if (!Regex.Match(textBox1.Text, "^[A-Z][a-zA-Z ]*$").Success)
+            {
+                MessageBox.Show("Uneli ste naziv automobila u pogresnom formatu. Stavite prvo slovo veliko.");
+                textBox1.Focus();
+                return;
+            }
+
+            if (!Regex.Match(textBox3.Text, "^[0-9]*$").Success)
+            {
+                MessageBox.Show("Uneli ste cenu automobila u pogresnom formatu. Morate uneti samo brojcanu vrednost.");
+                textBox3.Focus();
+                return;
+            }
+
+            Car c = new Car();
+            c.GetSetNameCar = textBox1.Text;
+            c.GetSetTypeCar = textBox2.Text;
+            c.GetSetPriceCar = Convert.ToInt32(textBox3.Text);
+
+            this.companyBusiness.InsertCars(c);
+
+            FillProductList();
+
+            textBox1.ResetText();
+            textBox2.ResetText();
+            textBox3.ResetText();
+        }
+
+
+        private void FillProductList()
+        {
+            listBox1.Items.Clear();
+
+            List<Car> car = companyBusiness.GetAllCars();
+
+            foreach (Car c in car)
+            {
+                listBox1.Items.Add("Automobil: " + c.GetSetNameCar + " - tip: " + c.GetSetTypeCar + " - cena: " + c.GetSetPriceCar);
+            }
+        }
+
+
+        private void FillCarListWithID()
+        {
+            listBox1.Items.Clear();
+
+            List<Car> car = companyBusiness.GetAllCars();
+
+            foreach (Car c in car)
+            {
+                listBox1.Items.Add("ID automobila: " + c.GetSetIdCar + " - automobil: " + c.GetSetNameCar + " - tip: " + c.GetSetTypeCar + " - cena: " + c.GetSetPriceCar);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Molim vas da popunite sva polja.");
+                textBox1.Focus();
+                return;
+            }
+
+            if (!Regex.Match(textBox1.Text, "^[A-Z][a-zA-Z ]*$").Success)
+            {
+                MessageBox.Show("Uneli ste naziv automobila u pogresnom formatu. Stavite prvo slovo veliko.");
+                textBox1.Focus();
+                return;
+            }
+
+            if (!Regex.Match(textBox3.Text, "^[0-9]*$").Success)
+            {
+                MessageBox.Show("Uneli ste cenu automobila u pogresnom formatu. Morate uneti samo brojcanu vrednost.");
+                textBox3.Focus();
+                return;
+            }
+
+            Car uc = new Car();
+            uc.GetSetIdCar = Convert.ToInt32(textBox4.Text);
+            uc.GetSetNameCar = textBox1.Text;
+            uc.GetSetTypeCar = textBox2.Text;
+            uc.GetSetPriceCar = Convert.ToInt32(textBox3.Text);
+
+            this.companyBusiness.UpdateCars(uc);
+
+            textBox1.ResetText();
+            textBox2.ResetText();
+            textBox3.ResetText();
+            textBox4.ResetText();
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FillCarListWithID();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int checker;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checker % 2 == 0)
+                textBox4.Enabled = true;
+            else
+                textBox4.Enabled = false;
+
+            checker++;
+        }
+
+        private void Forma1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Car dc = new Car();
+            dc.GetSetIdCar = Convert.ToInt32(textBox4.Text);
+
+            this.companyBusiness.DeleteCars(dc);
+
+            textBox4.ResetText();
+        }
+
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checker % 2 == 0)
+            {
+                button3.Enabled = true;
+                textBox4.Enabled = true;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                checkBox1.Enabled = false;
+            }
+            else
+            {
+                button3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                checkBox1.Enabled = true;
+            }
+            checker++;
+        }
+
+        private void Forma1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
