a9cb3cb7813d206bddc87b264c41a720269a332f
VioletProject <98975291+VioletProject@users.noreply.github.com>
2022-02-07 20:04:44 +0100

    Add files via upload

---
+++ BusinessLayer/CompanyBusiness.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Models;
+
+namespace BusinessLayer
+{
+    public class CompanyBusiness
+    {
+        private CompanyRepository companyRepository;
+
+
+        public CompanyBusiness()
+        {
+            this.companyRepository = new CompanyRepository();
+        }
+
+
+        public List<Car> GetAllCars()
+        {
+            return this.companyRepository.GetAllCars();
+        }
+
+
+        public int InsertCars(Car ic)
+        {
+            return this.companyRepository.InsertCars(ic);
+        }
+        
+
+        public int UpdateCars(Car uc)
+        {
+            return this.companyRepository.UpdateCars(uc);
+        }
+
+        
+        public int DeleteCars(Car dc)
+        {
+            return this.companyRepository.DeleteCars(dc);
+        }
+
+
+    }
+}
